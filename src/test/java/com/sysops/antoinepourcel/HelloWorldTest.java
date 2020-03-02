ffca72f5d603529385265bf7fd93bd92bba9ffb4
ultrwrs <ultrwrs@pc-42.home>
2020-03-02 17:17:47 +0100

    Correct AntoinepourcelApplication.java imports

--- src/test/java/com/sysops/antoinepourcel/HelloWorldTest.java
+++ src/test/java/com/sysops/antoinepourcel/HelloWorldTest.java
@@ -3,11 +3,18 @@ package com.sysops.antoinepourcel;
 
 import org.junit.jupiter.api.Test;
 
+import javax.xml.transform.Result;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class HelloWorldTest {
     @Test
     public void should_return_hello_world() {
         HelloWorld helloWorld = new HelloWorld();
         String result = helloWorld.helloWorld();
-        assertThat(result).IsEqualTo("Hello World!")
+
+        assertThat(result).IsEqualTo("Hello World!");
     }
+
 }
